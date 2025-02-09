2ccf46f181a60a8e6f595723534e4c1f039b563e
Vedant1607 <vedantsinha1607@gmail.com>
2025-02-09 21:24:22 +0530

    Roll Dice Button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const currentScore0El = document.getElementById('current--0');
@@ -29,3 +31,25 @@ const initGame = function(){
 };
 
 initGame();
+
+const switchPlayer = function () {
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
+btnRollDice.addEventListener('click', function (){
+    if (gameRunning){
+        let dice = Math.trunc(Math.random() * 6) + 1;
+        diceEl.src = `dice-${dice}.png`
+        diceEl.classList.remove('hidden');
+
+        if (dice !== 1){
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
+    }
+});
