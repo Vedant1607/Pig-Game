649da5872eace99f2b30903cd7f58921e2c20674
Vedant1607 <vedantsinha1607@gmail.com>
2025-02-09 21:06:22 +0530

    Add Buttons

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const currentScore0El = document.getElementById('current--0');
 const currentScore1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+
+// Buttons
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Initialize values
 let score, currentScore, gameRunning, activePlayer;
@@ -18,6 +24,8 @@ const initGame = function(){
     score1El.textContent = 0;
     currentScore0El.textContent = 0;
     currentScore1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
 };
 
 initGame();
