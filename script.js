702fa576cffdabcb4663af4c444b78c01cd462ae
Vedant1607 <vedantsinha1607@gmail.com>
2025-02-09 21:27:16 +0530

    Fix Bug

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ initGame();
 
 const switchPlayer = function () {
     currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
